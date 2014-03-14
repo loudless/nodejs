123fb015e3ad930d3883a58efa2a12867282d030
radaev <radaev@adv.ru>
2014-03-14 13:05:48 +0400

    index.js repair

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// var server = require('./server');
-// var router  = require('./router');
-// var requestHandlers = require('./requestHandlers');
+var server = require('./server');
+var router  = require('./router');
+var requestHandlers = require('./requestHandlers');
 
-// var handle = {};
-// handle['/'] = requestHandlers.start;
-// handle['/start'] = requestHandlers.start;
-// handle['/upload'] = requestHandlers.upload;
-// handle['/show'] = requestHandlers.show;
+var handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+handle['/show'] = requestHandlers.show;
 
-// server.start(router.route, handle);
+server.start(router.route, handle);
 
 
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+// // web.js
+// var express = require("express");
+// var logfmt = require("logfmt");
+// var app = express();
 
-app.use(logfmt.requestLogger());
+// app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// var port = Number(process.env.PORT || 5000);
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
