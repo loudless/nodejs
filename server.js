07c8741697cc462c68640207c7f8f3acf09358f7
radaev <radaev@adv.ru>
2014-03-14 12:48:02 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var formidable = require("formidable"),
+	http = require("http"),
+	url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log('request '+ pathname +' recieved');
+		route(handle, pathname, response, request);
+	}
+
+	http.createServer(onRequest).listen(3002);
+	console.log('server is started');
+}
+
+exports.start = start;
